8a1d019624bbfeb04f70fe2333ebb9db86c79cc5
Washington-dev <washingtonbrasilino765@gmail.com>
2026-09-19 15:14:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
+@app.get("/")
+def read_root():
+    return {"status": "ok", "mensagem": "O servidor FastAPI está a rodar perfeitamente na nuvem!"}
 
 # ==========================================
 # MODELOS PYDANTIC
