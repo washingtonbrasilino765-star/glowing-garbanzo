0b95d5510e79194b7c0dd65840cea936bd87989b
Washington-dev <washingtonbrasilino765@gmail.com>
2026-09-19 14:08:44 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,14 +1,22 @@
+import os
+
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-# Aqui é onde trocamos o destino! Saímos do SQLite e vamos para o PostgreSQL
-# Formato: postgresql://usuario:senha@localhost:porta/nome_do_banco
-SQLALCHEMY_DATABASE_URL = "postgresql://neondb_owner:****************@ep-billowing-morning-b5d2is71-pooler.c-7.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 
-# Para PostgreSQL não precisamos do "check_same_thread" que o SQLite exigia
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not SQLALCHEMY_DATABASE_URL:
+    raise RuntimeError("DATABASE_URL não foi configurada no ambiente.")
+
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
