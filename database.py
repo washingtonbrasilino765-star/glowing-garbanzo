b3c33c4536614e81b19b7205d2a0b86349f65e69
Washington-dev <washingtonbrasilino765@gmail.comgit commit -m Meu primeiro envio>
2026-09-24 01:57:29 -0300

    Envia requirements corrigido e salvo

--- database.py
+++ database.py
@@ -1,14 +1,9 @@
 import os
-<<<<<<< HEAD
 from dotenv import load_dotenv
-=======
-
->>>>>>> 5e5191596b512985544da58cd721e02ff91c677c
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-<<<<<<< HEAD
 # 1. Carrega as variáveis de ambiente declaradas no seu ficheiro .env
 load_dotenv()
 
@@ -23,25 +18,3 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # 5. Instância base para criação dos modelos/tabelas
 Base = declarative_base()
-=======
-
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
-
-if not SQLALCHEMY_DATABASE_URL:
-    raise RuntimeError("DATABASE_URL não foi configurada no ambiente.")
-
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    pool_pre_ping=True,
-    pool_recycle=300
-)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
-Base = declarative_base()
->>>>>>> 5e5191596b512985544da58cd721e02ff91c677c
